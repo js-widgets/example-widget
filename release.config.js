a2b6c6a164a3168a71b8c847a5f01d07f04298a2
Mateu Aguiló Bosch <mateu@mateuaguilo.com>
2020-10-09 00:15:24 +0200

    ci: add semantic release configuration
    This will avoid the NPM publication.

--- release.config.js
+++ release.config.js
@@ -2,5 +2,9 @@ module.exports = {
   branches: ['master'],
   analyzeCommits: 'semantic-release-conventional-commits',
   repositoryUrl: 'https://github.com/js-widgets/example-widget.git',
-  plugins: ['@semantic-release/commit-analyzer', '@semantic-release/github'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/github',
+  ],
 };
